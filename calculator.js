3b23d86b9f4d02234270c45e51ada5e8b76c85bd
Hugo Amaya <vamaya.sp@mysummitps.org>
2016-12-16 22:37:55 +0000

    update

--- calculator.js
+++ calculator.js
@@ -4,36 +4,37 @@ var num2input = document.getElementById("num2");
 var calculate = document.getElementById("calculate");
 var display = document.getElementById("display");
 //Above it describes the variables that are neccesary for the javascript to work 
+//The event listener bellow makes the calculate button function, and calculate with the "if" statements
 calculate.addEventListener("click", function(){
     var num1 = num1input.value;
     var num2 = num2input.value;
+//This simplifies the input value into more abriviated variables that can be written into the "if" statements 
     
-    
-//The following if statements are the various functions that the calculator will need in order to perform. 
+//The following "if" statements are the various functions that the calculator will need in order to perform. 
 //The selector value determines if they either do addition, subtraction, division, multiplication, the square root, or having number do the power of. 
     if(selector.value === "+"){
         var answera = +num1 + +num2;
         display.innerHTML = answera;
-    }
+    }//This is addition
     else if(selector.value === "-"){
         var answerb = num1 - num2;
         display.innerHTML = answerb;
-    }
+    }//This is subtraction
     else if(selector.value === "*"){
         var answerc = num1 * num2;
         display.innerHTML = answerc;
-    }
+    }//This is multiplication
     else if(selector.value === "/"){
         var answerd = num1 / num2;
         display.innerHTML = answerd;
-    }
+    }//This is division
     else if(selector.value === "^"){
         var answere = Math.pow(num1,num2);
         display.innerHTML = answere;
-    }
+    }//This is to the power of 
     else if(selector.value === "√"){
         var answerf = Math.sqrt(num2);
         display.innerHTML = answerf;
-    }
+    }//And this is the square root 
     
 });
--- todo2.js
+++ todo2.js
@@ -2,8 +2,8 @@ var usrTask = document.getElementById("usrTask");
 var textstyle = document.getElementById("textstyle");
 var display = document.getElementById("display");
 var submitBtn = document.getElementById("submitBtn");
-
-
+//These are variables that are determine what is going to be put into the todo list, they are necessary to make the todo list work each variable is unique make sure that the name is different.
+//Bellow there has been an event listener set up that makes the button work, it also tells the button to wrk when there is a click. 
 submitBtn.addEventListener("click", function(){
 
    var newTask = usrTask.value;
@@ -24,7 +24,7 @@ submitBtn.addEventListener("click", function(){
         display.innerHTML += '<div style="font: italic 15px/30px Times New Roman, serif;">' +newTask+ '</div>';
     }
 });
-
+//This event listener is a target that 
 display.addEventListener("click", function(evt){
     evt.target.parentNode.removeChild(evt.target);
 });
